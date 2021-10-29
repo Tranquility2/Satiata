75ac11705846dd54dd475172f6c47f2005184bff
Roy Moore <roy@moore.co.il>
2021-10-30 00:42:32 +0300

    added font

--- StatusText.gd
+++ StatusText.gd
@@ -7,7 +7,7 @@ extends RichTextLabel
 func UpdateMoves():
 	var text_msg: String =  Globals.format_string.format({"current": Globals.move_counter, "max": Globals.MAX_MOVES})
 	if Globals.move_counter < 21:
-		text_msg += ", Select color"
+		text_msg += ", Please select next color"
 	self.text = text_msg
 
 func UpdateWin():
